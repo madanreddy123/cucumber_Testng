27843157c7ddbadfb9c1b78899cf0e0e3b225fd4
madan <madanreddy319@gmail.com>
2021-09-11 18:13:15 +0100

    cucumber testng

---
+++  reports/report.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/Features/login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Admin Login",
+  "description": "",
+  "id": "admin-login",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 7376433460,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Enter the education form",
+  "description": "",
+  "id": "admin-login;enter-the-education-form",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter the name a Primary_School",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter the name a Secondary_School",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter the name a Intermediate",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter the name a Engineering",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter the name a University",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter the intermediate_Percentage",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "I click on gender",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Stepsdef.i_enter_the_name_a_Primary_School()"
+});
+formatter.result({
+  "duration": 236841085,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepsdef.i_enter_the_name_a_Secondary_School()"
+});
+formatter.result({
+  "duration": 68987819,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepsdef.i_enter_the_name_a_Intermediate()"
+});
+formatter.result({
+  "duration": 70404197,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepsdef.i_enter_the_name_a_Engineering()"
+});
+formatter.result({
+  "duration": 68968386,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepsdef.i_enter_the_name_a_University()"
+});
+formatter.result({
+  "duration": 88444604,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepsdef.i_enter_the_intermediate_Percentage()"
+});
+formatter.result({
+  "duration": 4203535991,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Stepsdef.i_click_on_gender()"
+});
+formatter.result({
+  "duration": 185384363,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 20160032,
+  "status": "passed"
+});
+});
---
+++ src/test/java/features/Login.feature
@@ -0,0 +1,13 @@
+Feature: Admin Login 
+
+Scenario: Enter the education form
+
+      Given I enter the name a Primary_School 
+      Given I enter the name a Secondary_School 
+      Given I enter the name a Intermediate 
+      When I enter the name a Engineering 
+      When I enter the name a University 
+      When I enter the intermediate_Percentage 
+      When  I click on gender
+
+
